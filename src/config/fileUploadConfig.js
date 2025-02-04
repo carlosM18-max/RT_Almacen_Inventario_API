430d71fc6b08497bd2f8c43931626ef46430b0f8
carlos <carloshernandez74199@gmail.com>
2025-02-04 15:27:41 -0600

    Pruebas
    Se realizaron las pruebas para la subida de archivos, se logro subir los archivos, pero produce un error

--- src/config/fileUploadConfig.js
+++ src/config/fileUploadConfig.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 // Configuración de almacenamiento de archivos generales
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (req, _file, cb) {
     cb(null, path.join(__dirname, "../public/uploads"));
   },
   filename: function (req, file, cb) {
@@ -20,7 +20,7 @@ const upload = multer({ storage });
 
 // Configuración de almacenamiento de archivos del usuario
 const storageUser = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (req, _file, cb) {
     cb(null, path.join(__dirname, "../public/users"));
   },
   filename: function (req, file, cb) {
@@ -32,17 +32,12 @@ const uploadUser = multer({ storage: storageUser });
 
 // Configuración de multer para los archivos de entregas
 const storageDeliveries = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadPath = path.resolve(__dirname, '../../public/deliveries');
-    // Crea la carpeta si no existe
-    fs.mkdirSync(uploadPath, { recursive: true });  
-    
-    cb(null, uploadPath);
+
+  destination: function (req, _file, cb) {
+    cb(null, path.join(__dirname, "../public/deliveries"));
   },
   filename: function (req, file, cb) {
-    // Sanitiza el nombre del archivo
-    const filename = file.originalname.replace(/[^\w\s.-]/g, '_');  
-    cb(null, `${Date.now()}-${filename}`); 
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
