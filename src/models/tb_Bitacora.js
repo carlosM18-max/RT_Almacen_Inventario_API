a4b980e884449c080c69ae307d7b9a7be4e764f5
Beto25-D <chichitobonito89@gmail.com>
2025-04-04 13:25:15 -0600

    Creacion de tabla bitacora

---
+++ src/models/tb_Bitacora.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import db from "../config/db.js";
+
+const Bitacora = db.define("tb_Bitacora", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tabla: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  operacion: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  descrpcion: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  usuario_responsable: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
+}, {
+  timestamps: false,
+});
+
+export default Bitacora;
